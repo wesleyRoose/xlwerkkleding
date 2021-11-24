d6b337345d266533e2e4b959db458f5b95f44285
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-11-24 15:10:57 +0100

    header
    header

--- php/footer.php
+++ php/footer.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<div style="clear: both;"></div>
-    <div class="footer-contact">
-        <div class="inner-content">
-            <p><i class="fas fa-phone"></i>06-1111111111 &nbsp;</p>
-            <p><i class="fas fa-envelope"></i>bunnywiki@info.nl</p>
-        </div>
-=======
         <script src="js/script.js"></script>
->>>>>>> 02d2ae547456f0da53c59e7b0d8bf6648c5df7b9
     </div>
     <div class="footer">
         <ul>
-        <p class="footer_text">&copy; BunnyWiki -
+        <p class="footer_text">&copy; XLwerkkleding -
             <script>var date = new Date(); var year = date.getFullYear(); document.write(year);</script> - Alle rechten voorbehouden
         </p>
         </ul>
