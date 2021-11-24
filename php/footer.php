1e2e1ac6e4902decb25884e529ee2a798b47d6f9
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-11-24 14:58:00 +0100

    footer
    footer

--- php/footer.php
+++ php/footer.php
@@ -1,3 +1,19 @@
+<div style="clear: both;"></div>
+    <div class="footer-contact">
+        <div class="inner-content">
+            <p><i class="fas fa-phone"></i>06-1111111111 &nbsp;</p>
+            <p><i class="fas fa-envelope"></i>bunnywiki@info.nl</p>
+        </div>
     </div>
+    <div class="footer">
+        <ul>
+        <p class="footer_text">&copy; BunnyWiki -
+            <script>var date = new Date(); var year = date.getFullYear(); document.write(year);</script> - Alle rechten voorbehouden
+        </p>
+        </ul>
+    </div>
+
+    <script src="../js/script.js"></script>
+    <script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
 </body>
 </html>
