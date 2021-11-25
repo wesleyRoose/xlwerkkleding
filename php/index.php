8853ef1ddbe107891562f0b6a9fc670e75386c8e
stfokkert <52315931+stfokkert@users.noreply.github.com>
2021-11-25 11:28:37 +0100

    alles is verkracht

--- php/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-include 'header.php';
-include 'main.php';
-include 'footer.php';
-
-?>
--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="nl">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,23 +8,26 @@
     <!--Custom CSS-->
     <link rel="stylesheet" href="../css/style.css">
     <!--FontAwesome-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>XLwerkkleding - Home</title>
 </head>
+
 <body>
     <div class="view">
         <header>
             <div class="header">
                 <div class="logo">
-                    <a class="logo-link" href="index.php"><h2>XLwerkkleding</h2></a>        
+                    <a class="logo-link" href="index.php">
+                        <h2>XLwerkkleding</h2>
+                    </a>
                 </div>
                 <nav class="menu" id="menu">
                     <div class="menu-wrap">
                         <ul class="menu-list">
-                            <li class="navitem"><a class="nav-item" href="../php/index.php">Home</a></li>  
-                            <li class="navitem"><a class="nav-item" href="categorie.php">Dropdown</a></li>
-                            <li class="navitem"><a class="nav-item" href="over.php">Over</a></li> 
-                            <li class="navitem"><a class="nav-item login" href="../php/login.php">Login</a></li> 
+                            <li class="navitem"><a class="nav-item" href="../php/index.php">Home</a></li>
+                            <li class="navitem"><a class="nav-item" href="categorie.php">WebShop</a></li>
+                            <li class="navitem"><a class="nav-item" href="over.php">Over</a></li>
+                            <li class="navitem"><a class="nav-item login" href="../php/login.php">Login</a></li>
                         </ul>
                     </div>
                 </nav>
