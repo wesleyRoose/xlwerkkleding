04dcb7c847bb4d3c06006c01efa624a0cec8bb1c
stfokkert <52315931+stfokkert@users.noreply.github.com>
2021-11-25 09:33:37 +0100

    Je bolle moeder home css

--- php/main.php
+++ php/main.php
@@ -1,73 +1,109 @@
 <main>
-    <section class="home-main">
-        <div class="home">
-            <h1>Welkom</h1>
-            <p>Slogan....</p>
+  <section class="mainFlexContainer">
+    <div class="home">
+      <h1>Welkom</h1>
+      <p>Slogan....</p>
+    </div>
+
+    <!-- Slideshow container -->
+    <div class="slideshowBoxContainer">
+      <div class="slideshow-container">
+
+        <div class="mySlides">
+          <div class="numbertext">1 / 3</div>
+          <img class="sliderImg" src="./img/placeholder.png">
+          <div class="sliderText">Caption Text</div>
         </div>
-        <div class="cards">
-            <div class="card">
-                <div class="image">
-                    <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
+
+        <div class="mySlides">
+          <div class="numbertext">2 / 3</div>
+          <img class="sliderImg" src="./img/placeholder.png">
+          <div class="sliderText">Caption Two</div>
         </div>
-    </section>
+
+        <div class="mySlides">
+          <div class="numbertext">3 / 3</div>
+          <img class="sliderImg" src="./img/placeholder.png">
+          <div class="sliderText">Caption Three</div>
+        </div>
+
+        <!-- Volgende en Vorige knoppen -->
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+      </div>
+    </div>
+    <br>
+
+    <!-- The punten en cirkels -->
+    <div style="text-align:center">
+      <span class="dot" onclick="currentSlide(1)"></span>
+      <span class="dot" onclick="currentSlide(2)"></span>
+      <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+    <div class="cards">
+      <div class="card">
+        <div class="image">
+          <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
+        </div>
+        <div class="text">
+          <h3>Product 1</h3>
+          <a href="#" class="card-link">Klik hier</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image">
+          <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
+        </div>
+        <div class="text">
+          <h3>Product 1</h3>
+          <a href="#" class="card-link">Klik hier</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image">
+          <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
+        </div>
+        <div class="text">
+          <h3>Product 1</h3>
+          <a href="#" class="card-link">Klik hier</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image">
+          <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
+        </div>
+        <div class="text">
+          <h3>Product 1</h3>
+          <a href="#" class="card-link">Klik hier</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image">
+          <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
+        </div>
+        <div class="text">
+          <h3>Product 1</h3>
+          <a href="#" class="card-link">Klik hier</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image">
+          <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
+        </div>
+        <div class="text">
+          <h3>Product 1</h3>
+          <a href="#" class="card-link">Klik hier</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="image">
+          <img src="img/placeholder.png" alt="Placeholder Image" class="card-image">
+        </div>
+        <div class="text">
+          <h3>Product 1</h3>
+          <a href="#" class="card-link">Klik hier</a>
+        </div>
+      </div>
+    </div>
+  </section>
 </main>
