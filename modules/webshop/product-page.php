81a9699471d5f9586da06107cda894a770c0182b
StefanMaring <smaring@ziggo.nl>
2021-11-30 13:54:10 +0100

    Product Page (Versie 1)
    Product Page

---
+++ modules/webshop/product-page.php
@@ -0,0 +1,38 @@
+<?php
+include("../../config.php");
+include("../../functions.php");
+
+include("../../templates/header.php");?>
+
+<main class="product-page">
+    <section class="product-page">
+        <div class="page-wrapper">
+            <div class="flex">
+                <div class="product-image">
+
+                </div>
+                <div class="product-info">
+                    <ul class="info-list">
+                        <li class="product-list-item">Leren Jas Bouw</li>
+                        <li class="product-list-item">Jassen</li>
+                        <li class="product-list-item">Bouw</li>
+                        <li class="product-list-item">Merk</li>
+                        <li class="product-list-item">$25</li>
+                        <li class="product-list-item">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                            Quae possimus magni provident 
+                            commodi saepe harum fuga facere 
+                            atque aspernatur dolore, 
+                            nostrum totam adipisci expedita, 
+                            veniam molestias. 
+                            Eius, totam fugiat! Delectus?
+                        </li>
+                        <button><a href="#" class="product-order-link">Bestel</a></button>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include("../../templates/footer.php");?>
