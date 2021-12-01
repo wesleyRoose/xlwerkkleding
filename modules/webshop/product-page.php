a5f53572fb5024999552b3bd917953863862e3f9
StefanMaring <smaring@ziggo.nl>
2021-12-01 13:25:12 +0100

    Product-Page v2

--- modules/webshop/product-page.php
+++ modules/webshop/product-page.php
@@ -13,21 +13,45 @@ include("../../templates/header.php");?>
                 </div>
                 <div class="product-info">
                     <ul class="info-list">
-                        <li class="product-list-item">Leren Jas Bouw</li>
-                        <li class="product-list-item">Jassen</li>
-                        <li class="product-list-item">Bouw</li>
-                        <li class="product-list-item">Merk</li>
-                        <li class="product-list-item">$25</li>
-                        <li class="product-list-item">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                            Quae possimus magni provident 
-                            commodi saepe harum fuga facere 
-                            atque aspernatur dolore, 
-                            nostrum totam adipisci expedita, 
-                            veniam molestias. 
-                            Eius, totam fugiat! Delectus?
-                        </li>
+                        <li class="product-list-item"><h3>Leren Jas Bouw</h3></li>
+                        <li class="product-list-item smaller">Jassen</li>
+                        <li class="product-list-item smaller">Bouw</li>
+                        <li class="product-list-item smaller">Merk</li>
+                        <li class="product-list-item smaller">$25</li>
+                        <div class="selects">
+                            <select name="sizes">
+                                <option value="S">S</option>
+                                <option value="M">M</option>
+                                <option value="L">L</option>
+                                <option value="XL">XL</option>
+                            </select>
+                            <select name="Kleuren">
+                                <option value="Blauw">Blauw</option>
+                                <option value="Rood">Rood</option>
+                                <option value="Geel">Geel</option>
+                                <option value="Groen">Groen</option>
+                            </select>
+                        </div>
                         <button><a href="#" class="product-order-link">Bestel</a></button>
+                        <div class="description">
+                            <h3 class="desc-title">Beschrijving</h3>
+                            <li class="product-list-item smaller">
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                                Quae possimus magni provident 
+                                commodi saepe harum fuga facere 
+                                atque aspernatur dolore, 
+                                nostrum totam adipisci expedita, 
+                                veniam molestias. 
+                                Eius, totam fugiat! Delectus?
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                                Quae possimus magni provident 
+                                commodi saepe harum fuga facere 
+                                atque aspernatur dolore, 
+                                nostrum totam adipisci expedita, 
+                                veniam molestias. 
+                                Eius, totam fugiat! Delectus?
+                            </li>
+                        </div>
                     </ul>
                 </div>
             </div>
