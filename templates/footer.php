39f09e858dba775782f1cf3a903570badbe18c0d
StefanMaring <smaring@ziggo.nl>
2021-12-02 09:49:59 +0100

    Footer Update
    Grote footer update die 2e balk toevoegt waar de klant om gevraagd had

--- templates/footer.php
+++ templates/footer.php
@@ -1,17 +1,58 @@
-        <footer>
-            <div class="footer">
-                <ul>
-                    <p class="footer_text">&copy; XLwerkkleding -
-                        <script>
-                            var date = new Date();
-                            var year = date.getFullYear();
-                            document.write(year);
-                        </script> - Alle rechten voorbehouden
-                    </p>
-                </ul>
+    <footer>
+        <div class="upper">
+            <div class="block">
+                <h3>Menu</h3>
+                <a class="footer-link lower" href="#">Home</a>
+                <a class="footer-link lower" href="#">Webshop</a>
+                <a class="footer-link lower" href="#">Over</a>
             </div>
-        </footer>
-        <script src="<?php echo ROOT_URL ?>js/script.js"></script>
+            <div class="block">
+                <h3>Contact</h3>
+                <div class="adress">Synagogestraat 7</div>
+                <div class="adress">7941 LZ Meppel</div>
+                <div class="adress">
+                    <a class="footer-link lower" href="tel:+31 (0)6 120 55775">+31 (0)6 120 55775</a>
+                </div>
+                <div class="adress">
+                    <a class="footer-link lower" href="mailto:info@bouwkledij.nl">info@bouwkledij.nl</a>
+                </div>
+            </div>
+            <div class="block">
+                <h3>Klantenservice</h3>
+                <a class="footer-link lower" href="#">Algemene Voorwaaden</a>
+                <a class="footer-link lower" href="#">Verzenden & Leveren</a>
+                <a class="footer-link lower" href="#">Klachten</a>
+                <a class="footer-link lower" href="#">Disclaimer</a>
+                    <!-- <div class="kvk">
+                        <p>KVK: &nbsp;82344348234</p>
+                        <p>BTW: NL584884848</p>
+                    </div> -->
+                </div>
+            <div class="block">
+                <h3>Betaalmethoden</h3>
+                <div class="payment-options">
+                    <img src="<?php echo ROOT_URL ?>img/betaal.png" alt="Payment Options Photo">
+                </div>
+            </div>
         </div>
-    </body>
+        <div class="bottom">
+            <div class="full-width">
+                <p class="footer_text">&copy; XLwerkkleding -
+                    <script>
+                        var date = new Date();
+                        var year = date.getFullYear();
+                        document.write(year);
+                    </script> - Alle rechten voorbehouden
+                </p>
+                <div class="footer-icons">
+                    <a href="#" class="footer-link"><i class="fab fa-facebook m-left"></i></a>
+                    <a href="#" class="footer-link"><i class="fab fa-linkedin m-left"></i></a>
+                    <a href="#" class="footer-link"><i class="fab fa-instagram m-left"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="<?php echo ROOT_URL ?>js/script.js"></script>
+</div>
+</body>
 </html>
