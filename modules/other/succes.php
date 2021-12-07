2797b35ce0d70372061c0dc3629418ed53810798
StefanMaring <smaring@ziggo.nl>
2021-12-07 13:55:00 +0100

    Succes page toegevoegd

---
+++ modules/other/succes.php
@@ -0,0 +1,14 @@
+<?php
+include("../../config.php");
+include("../../functions.php");
+?>
+
+<head>
+    <link rel="stylesheet" href="<?php echo ROOT_URL ?>css/style.css">
+</head>
+<section class="succes">
+    <div class="centered">  
+        <h1>Product(en) succesvol geüpload</h1>
+        <button class="button"><a class="btn-s" href="<?php echo ROOT_URL ?>modules/add-product.php">GA TERUG</a></button>
+    </div>
+</section>
