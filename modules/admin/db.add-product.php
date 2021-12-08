d8b24bd73868e4364da37d888f52912751dcfbee
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-12-08 13:59:37 +0100

    h
    h

--- modules/admin/db.add-product.php
+++ modules/admin/db.add-product.php
@@ -31,7 +31,7 @@ if (isset($_POST["addProductButton"])) {
 
 
     // PLACEHOLDER MUST REPLACE
-    $p_foto = $_POST["p_foto"];
+    $p_foto = $_POST["foto"];
 
     // Clean inputs for not wanted characters
     $p_name = cleaninput($_POST['p_name'], 20);
