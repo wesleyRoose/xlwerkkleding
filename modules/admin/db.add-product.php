740d37591e69078d5e4a706e8cdabeebfe3ae369
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-12-16 09:50:46 +0100

    stefan  f heeft eraan gewerkt
    stefan  f heeft eraan gewerkt

--- modules/admin/db.add-product.php
+++ modules/admin/db.add-product.php
@@ -44,7 +44,7 @@ if ($conn->connect_error) {
 // sql to create table
 $sql = "CREATE TABLE 'product' (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-$p_name VARCHAR(100) NOT NULL,
+p_name VARCHAR(100) NOT NULL,
 p_price int(5) NOT NULL,
 p_category VARCHAR(75)NOT NULL, 
 p_sector VARCHAR(100) NOT NULL,
@@ -56,15 +56,15 @@ reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 
 $sql = "INSERT INTO product (p_name, p_price, p_category, p_sector, p_brand, p_size, p_color, p_description )
-VALUES ('p_name', 'p_price', 'p_category', 'p_sector', 'p_brand', 'p_size', 'p_color', 'p_description')";
+VALUES (?,?,?,?,?,?,?,?)";
 
-if ($conn->query($sql) === TRUE) {
-  echo "Table product created successfully";
-} else {
-  echo "Error creating table: " . $conn->error;
-}
+// if ($conn->query($sql) === TRUE) {
+//   echo "Table product created successfully";
+// } else {
+//   echo "Error creating table: " . $conn->error;
+// }
 
-$conn->close();
+
 
 
   // Check if button is pressed
@@ -74,9 +74,9 @@ $conn->close();
 
 
 
-      // PLACEHOLDER MUST REPLACE
-      var_dump($_POST["p_file"]);
-      $p_foto = $_POST["p_file"];
+      // // PLACEHOLDER MUST REPLACE
+      // var_dump($_POST["p_file"]);
+      // $p_foto = $_POST["p_file"];
 
       // Clean inputs for not wanted characters
       $p_name = cleaninput($_POST['p_name'], 100);
@@ -97,62 +97,62 @@ $conn->close();
       $sLocationFailure = "";
       $sLocationFailure = 'Location: ' . ROOT_URL . 'modules/other/failure.php';
 
+      $p_foto = "Test";
       // Create Arrays for function parameters
       $aRowNames = array("p_name", "p_price", "p_category", "p_sector", "p_brand", "p_size", "p_color", "p_description", "p_foto");
 
       $aValues = array($p_name, $p_price, $p_category, $p_sector, $p_brand, $p_size, $p_color, $p_description, $p_foto);
-
-      if (preparedInsertIntoQuery($conn, 9, "product", $aRowNames, "sissssssb", $aValues)) {
+      if (preparedInsertIntoQuery($conn, 9, "product", $aRowNames, "sissssss", $aValues)) {
         header($sLocationSucces);
       } else {
         // header($sLocationFailure);
-        echo preparedInsertIntoQuery($conn, 9, "product", $aRowNames, "sissssssb", $aValues);
+        echo preparedInsertIntoQuery($conn, 9, "product", $aRowNames, "sisssssss", $aValues);
       }
     }
   }
 
 
 
-  //file upload directory
-  $targetDir = "./img/product_images/";
+  // //file upload directory
+  // $targetDir = "./img/product_images/";
 
-  if (isset($_POST["addProductButton"])) {
-    if (!empty($_FILES["file"]["product_images"])) {
-      $fileName = basename($_FILES["file"]["product_images"]);
-      $targetFilePath = $targetDir . $fileName;
-      $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+  // if (isset($_POST["addProductButton"])) {
+  //   if (!empty($_FILES["file"]["product_images"])) {
+  //     $fileName = basename($_FILES["file"]["product_images"]);
+  //     $targetFilePath = $targetDir . $fileName;
+  //     $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
 
-      //Allow certain file format
+  //     //Allow certain file format
 
-      $allowTypes = array('jpg', 'png', 'gif', 'pdf');
-      if (in_array($fileType, $allowTypes)) {
+  //     $allowTypes = array('jpg', 'png', 'gif', 'pdf');
+  //     if (in_array($fileType, $allowTypes)) {
 
-        //upload file to server
-        if (move_uploaded_file($_FILES["img"]["product_images"], $targetFilePath)) {
+  //       //upload file to server
+  //       if (move_uploaded_file($_FILES["img"]["product_images"], $targetFilePath)) {
 
-          //insert image file name into database
-          $insert = $db->query("INSERT into images (file_name, uploaded_on) VALUES ('" . $fileName . "', NOW())");
-          if ($insert) {
-            $statusMsg = "The file " . $fileName . " has been uploaded successfully.";
-          } else {
-            $statusMsg = "File upload failed, please try again.";
-          }
-        } else {
-          $statusMsg = "Sorry, there was an error uploading your file.";
-        }
-      } else {
-        $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
-      }
-    } else {
-      $statusMsg = 'Please select a file to upload.';
-    }
-  } else {
-    echo "Hello World";
-  }
+  //         //insert image file name into database
+  //         $insert = $db->query("INSERT into images (file_name, uploaded_on) VALUES ('" . $fileName . "', NOW())");
+  //         if ($insert) {
+  //           $statusMsg = "The file " . $fileName . " has been uploaded successfully.";
+  //         } else {
+  //           $statusMsg = "File upload failed, please try again.";
+  //         }
+  //       } else {
+  //         $statusMsg = "Sorry, there was an error uploading your file.";
+  //       }
+  //     } else {
+  //       $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
+  //     }
+  //   } else {
+  //     $statusMsg = 'Please select a file to upload.';
+  //   }
+  // } else {
+  //   echo "Hello World";
+  // }
 
-  // Display status message
-  if (isset($statusMsg)) {
-    echo $statusMsg;
-  }
+  // // Display status message
+  // if (isset($statusMsg)) {
+  //   echo $statusMsg;
+  // }
 }
 
