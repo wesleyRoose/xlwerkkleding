4d056a3e85f584c0884ff0d345669125929dc3c8
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2021-12-16 13:27:11 +0100

    shopping cart
    shopping cart

---
+++ modules/shopping-cart/index1.php
@@ -0,0 +1,57 @@
+<?php
+
+session_start();
+
+    //config page
+    if (file_exists('../../config.php')) {
+        include('../../config.php');
+    } else {
+        $errorMessage = "";
+        $errorMessage .= "PHP ERROR: config.php does not exist.";
+        echo $errorMessage;
+        exit;
+    }
+
+        // functions page
+    if (file_exists('../../functions.php')) {
+        include('../../functions.php');
+    } else {
+        $errorMessage = "";
+        $errorMessage .= "PHP ERROR: functions.php does not exist.";
+        echo $errorMessage;
+        exit;
+    }
+
+
+    if ($_SESSION["sessionStatus"] != 2 || empty($_SESSION["sessionStatus"])) {
+        header('Location: ' . ROOT_URL . 'index.php');
+    } else {
+
+        //header
+        if ($_SESSION["sessionStatus"] == 1) {
+            include "../../templates/header-user.php";
+        } else if ($_SESSION["sessionStatus"] == 2) {
+            include "../../templates/header-admin.php";
+        } else if ($_SESSION["sessionStatus"] == 6 || empty($_SESSION["sessionStatus"])) {
+            include "../../templates/header.php";
+        }
+    }
+?>
+
+<!-- begin pagina -->
+<div class="shopping-field">
+    <div class="cart-name">
+        <p> Winkelwagen</p>
+    </div>
+</div>
+
+<?php
+
+if ($_SESSION["sessionStatus"] == 1) {
+    include "../../templates/footer-user.php";
+} else if ($_SESSION["sessionStatus"] == 2) {
+    include "../../templates/footer-admin.php";
+} else if ($_SESSION["sessionStatus"] == 6 || empty($_SESSION["sessionStatus"])) {
+    include "../../templates/footer.php";
+}
+?>
