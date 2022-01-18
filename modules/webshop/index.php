cdfbb31cbad8e07a049cc6f825ef8d2c31b60ec1
stfokkert <52315931+stfokkert@users.noreply.github.com>
2022-01-18 13:08:37 +0100

    shit

--- modules/webshop/index.php
+++ modules/webshop/index.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if(!isset($_SESSION["sessionStatus"])){
+if (!isset($_SESSION["sessionStatus"])) {
   $_SESSION["sessionStatus"] = 0;
 }
 
@@ -36,167 +36,183 @@ if ($_SESSION["sessionStatus"] == 1) {
 ?>
 
 <main class="webshop">
-    <section class="webshop">
-        <div class="page-wrapper">
-          <h1 class="title">Webshop</h1>
-            <div class="filter-bar">
-              <div class="select-container">
-                <div class="label-check">
-                  <label>Types</label>
-                  <div class="check-box">
-                    <input type="checkbox">
-                  </div>
-                </div>
-                <select name="types" class="webshop-filter-select">
-                    <option value="Jassen">Jassen</option>
-                    <option value="Overallen">Overallen</option>
-                    <option value="Broeken">Broeken</option>
-                  <option value="Overige">Overige</option>
-                </select>
-              </div>
-              <div class="select-container">
-                <div class="label-check">
-                  <label>Sector</label>
-                  <div class="check-box">
-                    <input type="checkbox">
-                  </div>
-                </div>
-                <select name="types" class="webshop-filter-select">
-                    <option value="Bouw">Bouw</option>
-                    <option value="Zorg">Zorg</option>
-                    <option value="Schoonmaak">Schoonmaak</option>
-                    <option value="Kantoor">Kantoor</option>
-                    <option value="Fabriek">Fabriek</option>
-                </select>
-              </div>
-              <div class="select-container">
-                <div class="label-check">
-                  <label>Merk</label>
-                  <div class="check-box">
-                    <input type="checkbox">
-                  </div>
-                </div>
-                <select name="types" class="webshop-filter-select">
-                    <option value="Merk1">Merk1</option>
-                    <option value="Merk2">Merk2</option>
-                    <option value="Merk3">Merk3</option>
-                    <option value="Merk4">Merk4</option>
-                    <option value="Merk5">Merk5</option>
-                </select>
-              </div>
-              <div class="select-container">
-                <div class="label-check">
-                  <label>Geslacht</label>
-                  <div class="check-box">
-                    <input type="checkbox">
-                  </div>
-                </div>
-                <select name="types" class="webshop-filter-select">
-                    <option value="Merk1">Man</option>
-                    <option value="Merk2">Vrouw</option>
-                </select>
-              </div>
-              <div class="select-container">
-                <input type="submit" class="button sub" value="Filter">
+  <section class="webshop">
+    <div class="page-wrapper">
+      <h1 class="title">Webshop</h1>
+      <form action="" method="post">
+        <div class="filter-bar">
+          <div class="select-container">
+            <div class="label-check">
+              <label>Types</label>
+              <div class="check-box">
+                <input type="checkbox" name="filterCheckBox" value="types" checked>
               </div>
             </div>
+            <select name="types" class="webshop-filter-select">
+              <option value="Jassen">Jassen</option>
+              <option value="Overallen">Overallen</option>
+              <option value="Broeken">Broeken</option>
+              <option value="Overige">Overige</option>
+            </select>
+          </div>
+          <div class="select-container">
+            <div class="label-check">
+              <label>Sector</label>
+              <div class="check-box">
+                <input type="checkbox" name="filterCheckBox" value="sector" checked>
+              </div>
+            </div>
+            <select name="types" class="webshop-filter-select">
+              <option value="Bouw">Bouw</option>
+              <option value="Zorg">Zorg</option>
+              <option value="Schoonmaak">Schoonmaak</option>
+              <option value="Kantoor">Kantoor</option>
+              <option value="Fabriek">Fabriek</option>
+            </select>
+          </div>
+          <div class="select-container">
+            <div class="label-check">
+              <label>Merk</label>
+              <div class="check-box">
+                <input type="checkbox" name="filterCheckBox" value="merk" checked>
+              </div>
+            </div>
+            <select name="types" class="webshop-filter-select">
+              <option value="Merk1">Merk1</option>
+              <option value="Merk2">Merk2</option>
+              <option value="Merk3">Merk3</option>
+              <option value="Merk4">Merk4</option>
+              <option value="Merk5">Merk5</option>
+            </select>
+          </div>
+          <div class="select-container">
+            <div class="label-check">
+              <label>Geslacht</label>
+              <div class="check-box">
+                <input type="checkbox" name="filterCheckBox" value="geslacht" checked>
+              </div>
+            </div>
+            <select name="types" class="webshop-filter-select">
+              <option value="Merk1">Man</option>
+              <option value="Merk2">Vrouw</option>
+            </select>
+          </div>
+          <div class="select-container">
+            <input type="submit" name="formSubmit" class="button sub" value="Filter">
+            <input type="submit" name="formReset" class="button sub" value="Reset">
+          </div>
         </div>
-    </section>
+      </form>
+    </div>
+  </section>
 
-    <section class="webshop-cards">
-    <div class="cards">
-            <div class="card">
-                <div class="image">
-                    <img src="<?php echo ROOT_URL ?>img/placeholder.png" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="<?php echo ROOT_URL ?>img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="<?php echo ROOT_URL ?>img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="<?php echo ROOT_URL ?>img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="<?php echo ROOT_URL ?>img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="<?php echo ROOT_URL ?>img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="<?php echo ROOT_URL ?>img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="<?php echo ROOT_URL ?>img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="<?php echo ROOT_URL ?>img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-            <div class="card">
-                <div class="image">
-                    <img src="<?php echo ROOT_URL ?>img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
-                </div>
-                <div class="text">
-                    <h3>Product 1</h3>
-                    <a href="#" class="card-link">Klik hier</a>
-                </div>
-            </div>
-        </div>
-    </section>
+  <section class="webshop-cards">
+
+    <?php
+
+    // Generating results from Search engine
+    if (isset($_POST["formSubmit"])) {
+      echo "test";
+    } else if (isset($_POST["formReset"])) {
+      $sHtml = '';
+      $sHtml = '<div class="cards">
+    <div class="card">
+      <div class="image">
+        <img src=" ' . ROOT_URL . 'img/placeholder.png" alt="Placeholder Image" class="card-image">
+      </div>
+      <div class="text">
+        <h3>Product 1</h3>
+        <a href="#" class="card-link">Klik hier</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src=" ' . ROOT_URL . 'img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
+      </div>
+      <div class="text">
+        <h3>Product 1</h3>
+        <a href="#" class="card-link">Klik hier</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src=" ' . ROOT_URL . 'img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
+      </div>
+      <div class="text">
+        <h3>Product 1</h3>
+        <a href="#" class="card-link">Klik hier</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src=" ' . ROOT_URL . 'img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
+      </div>
+      <div class="text">
+        <h3>Product 1</h3>
+        <a href="#" class="card-link">Klik hier</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src=" ' . ROOT_URL . 'img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
+      </div>
+      <div class="text">
+        <h3>Product 1</h3>
+        <a href="#" class="card-link">Klik hier</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src=" ' . ROOT_URL . 'img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
+      </div>
+      <div class="text">
+        <h3>Product 1</h3>
+        <a href="#" class="card-link">Klik hier</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src=" ' . ROOT_URL . 'img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
+      </div>
+      <div class="text">
+        <h3>Product 1</h3>
+        <a href="#" class="card-link">Klik hier</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src=" ' . ROOT_URL . 'img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
+      </div>
+      <div class="text">
+        <h3>Product 1</h3>
+        <a href="#" class="card-link">Klik hier</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src=" ' . ROOT_URL . 'img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
+      </div>
+      <div class="text">
+        <h3>Product 1</h3>
+        <a href="#" class="card-link">Klik hier</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src=" ' . ROOT_URL . 'img/koudblauw.jpg" alt="Placeholder Image" class="card-image">
+      </div>
+      <div class="text">
+        <h3>Product 1</h3>
+        <a href="#" class="card-link">Klik hier</a>
+      </div>
+    </div>
+  </div>';
+      echo $sHtml;
+    }
+
+    ?>
+
+  </section>
 </main>
 
 <?php
