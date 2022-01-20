778d888c4da6e6cca21dad57783c400966021ec3
stfokkert <52315931+stfokkert@users.noreply.github.com>
2022-01-20 12:38:44 +0100

    Alstu
    Documentatie, paar foto's, product-page en webshop index

--- modules/webshop/index.php
+++ modules/webshop/index.php
@@ -97,77 +97,80 @@ if ($_SESSION["sessionStatus"] == 1) {
   </section>
 
   <section class="webshop-cards">
+    <div class="cards">
 
-    <?php
+      <?php
 
 
-    $sDefaultHtml = '';
+      $sDefaultHtml = '';
 
-    // Generating results from Search engine
-    // Check if formSubmit is pressed
-    if (isset($_POST["formSubmit"])) {
-      // Check how many and which filters are selected
-      if (isset($_POST["filterCheckBox"])) {
-        $aFilterCheckBoxes = $_POST["filterCheckBox"];
+      // Generating results from Search engine
+      // Check if formSubmit is pressed
+      if (isset($_POST["formSubmit"])) {
 
         // Creating arrays for later use in the query
         $aQueryData = array();
 
-        // Array for brands
-        // Check if `brand` is selected
-        if (in_array("brand", $aFilterCheckBoxes)) {
-          $aBrand = array();
-          if (isset($_POST["brand"])) {
-            $aBrand = array("p_brand", $_POST["brand"]);
-            // Add array to complete array
-            array_push($aQueryData, $aBrand);
-          }
-        }
+        // Check how many and which filters are selected
+        if (isset($_POST["filterCheckBox"])) {
+          $aFilterCheckBoxes = $_POST["filterCheckBox"];
 
-        // Array for Categories
-        // Check if `categories` is selected
-        if (in_array("categories", $aFilterCheckBoxes)) {
-          $aCategories = array();
-          if (isset($_POST["categories"])) {
-            $aCategories = array("p_category", $_POST["categories"]);
-            // Add array to complete array
-            array_push($aQueryData, $aCategories);
+          // Array for brands
+          // Check if `brand` is selected
+          if (in_array("brand", $aFilterCheckBoxes)) {
+            $aBrand = array();
+            if (isset($_POST["brand"])) {
+              $aBrand = array("p_brand", $_POST["brand"]);
+              // Add array to complete array
+              array_push($aQueryData, $aBrand);
+            }
           }
-        }
 
-        // Array for sector
-        // Check if `sector` is selected
-        if (in_array("sector", $aFilterCheckBoxes)) {
-          $aSector = array();
-          if (isset($_POST["sector"])) {
-            $aSector = array("p_sector", $_POST["sector"]);
-            // Add array to complete array
-            array_push($aQueryData, $aSector);
+          // Array for Categories
+          // Check if `categories` is selected
+          if (in_array("categories", $aFilterCheckBoxes)) {
+            $aCategories = array();
+            if (isset($_POST["categories"])) {
+              $aCategories = array("p_category", $_POST["categories"]);
+              // Add array to complete array
+              array_push($aQueryData, $aCategories);
+            }
           }
-        }
 
-        // print_r($aQueryData);
-        // Check if array is not empty, if so skip first query building
-        if (!empty($aQueryData)) {
+          // Array for sector
+          // Check if `sector` is selected
+          if (in_array("sector", $aFilterCheckBoxes)) {
+            $aSector = array();
+            if (isset($_POST["sector"])) {
+              $aSector = array("p_sector", $_POST["sector"]);
+              // Add array to complete array
+              array_push($aQueryData, $aSector);
+            }
+          }
+
+          // Check if array is not empty, if so skip first query building
           // Get length of Array for query building
           $iFilterLength = count($aQueryData);
 
-
-          // $query = "SELECT * FROM `product` WHERE `p_id` = 1 AND `p_name` = 'test'";
-
-
-
+          // Check how long the Filter is
           if ($iFilterLength == 1) {
+            // Create Query based on the length of filter
             $sQuery = "SELECT * FROM `product` WHERE `" . $aQueryData[0][0] . "` = '" . $aQueryData[0][1] . "'";
+            // Create Query based on the length of filter
           } else if ($iFilterLength == 2) {
+            // Create Query based on the length of filter
             $sQuery = "SELECT * FROM `product` WHERE `" . $aQueryData[0][0] . "` = '" . $aQueryData[0][1] . "' AND `" . $aQueryData[1][0] . "` = '" . $aQueryData[1][1] . "'";
           } else if ($iFilterLength == 3) {
+            // Create Query based on the length of filter
             $sQuery = "SELECT * FROM `product` WHERE `" . $aQueryData[0][0] . "` = '" . $aQueryData[0][1] . "' AND `" . $aQueryData[1][0] . "` = '" . $aQueryData[1][1] . "' AND `" . $aQueryData[2][0] . "` = '" . $aQueryData[2][1] . "'";
           }
+
+          // Execute Query on database connection and put the data into a Array
           if ($oResult = $conn->query($sQuery)) {
+            // Generate Product Cards
             while ($aRow = $oResult->fetch_assoc()) {
-              $sDefaultHtml .= '<div class="cards">
-      <div class="card">
+              $sDefaultHtml .= '
+    <div class="card">
       <div class="image">
         <img src=" ' . ROOT_URL . 'modules/admin/' . $aRow["p_foto"] . '" alt="Placeholder Image" class="card-image">
       </div>
@@ -176,26 +179,80 @@ if ($_SESSION["sessionStatus"] == 1) {
         <p>' . $aRow["p_sector"] . '</p>
         <a href="' . ROOT_URL . "modules/webshop/product-page.php?productId=" . $aRow["p_id"] . '" class="card-link">Klik hier</a>
       </div>
-      </div>';
+    </div>';
             }
-          } else {
+          }
+        } else { // If no boxes are selected for the filter, select all
+          // Create Query String
+          $sQuery = "SELECT * FROM `product`";
+          // Execute Query on database connection and put the data into a Array
+          if ($oResult = $conn->query($sQuery)) {
+            // Generate Product Cards
+            while ($aRow = $oResult->fetch_assoc()) {
+              $sDefaultHtml .= '
+<div class="card">
+  <div class="image">
+    <img src=" ' . ROOT_URL . 'modules/admin/' . $aRow["p_foto"] . '" alt="Placeholder Image" class="card-image">
+  </div>
+  <div class="text">
+    <h3>' . $aRow["p_name"] . '</h3>
+    <p>' . $aRow["p_sector"] . '</p>
+    <a href="' . ROOT_URL . "modules/webshop/product-page.php?productId=" . $aRow["p_id"] . '" class="card-link">Klik hier</a>
+  </div>
+</div>';
+            }
+          }
+        }
+
+        // Check if formReset is pressed
+      } else if (isset($_POST["formReset"])) {
+        // Create Query String
+        $sQuery = "SELECT * FROM `product`";
+        // Execute Query on database connection and put the data into a Array
+        if ($oResult = $conn->query($sQuery)) {
+          // Generate Product Cards
+          while ($aRow = $oResult->fetch_assoc()) {
+            $sDefaultHtml .= '
+<div class="card">
+  <div class="image">
+    <img src=" ' . ROOT_URL . 'modules/admin/' . $aRow["p_foto"] . '" alt="Placeholder Image" class="card-image">
+  </div>
+  <div class="text">
+    <h3>' . $aRow["p_name"] . '</h3>
+    <p>' . $aRow["p_sector"] . '</p>
+    <a href="' . ROOT_URL . "modules/webshop/product-page.php?productId=" . $aRow["p_id"] . '" class="card-link">Klik hier</a>
+  </div>
+</div>';
+          }
+        }
+        // Check if nothing is pressed
+      } else {
+        // Create Query String
+        $sQuery = "SELECT * FROM `product`";
+        // Execute Query on database connection and put the data into a Array
+        if ($oResult = $conn->query($sQuery)) {
+          // Generate Products Cards
+          while ($aRow = $oResult->fetch_assoc()) {
+            $sDefaultHtml .= '
+<div class="card">
+  <div class="image">
+    <img src=" ' . ROOT_URL . 'modules/admin/' . $aRow["p_foto"] . '" alt="Placeholder Image" class="card-image">
+  </div>
+  <div class="text">
+    <h3>' . $aRow["p_name"] . '</h3>
+    <p>' . $aRow["p_sector"] . '</p>
+    <a href="' . ROOT_URL . "modules/webshop/product-page.php?productId=" . $aRow["p_id"] . '" class="card-link">Klik hier</a>
+  </div>
+</div>';
           }
         }
       }
-
-
-
+      // Finish Html <div>
+      $sDefaultHtml .= "</div>";
+      // Print Html on screen
       echo $sDefaultHtml;
 
-      // Check if formReset is pressed
-    } else if (isset($_POST["formReset"])) {
-      echo $sDefaultHtml;
-      // Check if nothing is pressed
-    } else {
-      echo $sDefaultHtml;
-    }
-
-    ?>
+      ?>
 
   </section>
 </main>
