d628c9ea86ea3d17a53d3d482c0c488c59c9392b
StefanMaring <smaring@ziggo.nl>
2022-01-20 08:56:28 +0100

    h

--- modules/shopping-cart/index.php
+++ modules/shopping-cart/index.php
@@ -1,67 +1,87 @@
 <?php
-//Start Session
+
 session_start();
 
-$_SESSION["aCart"] = array(1, 2, 4);
+    //config page
+    if (file_exists('../../config.php')) {
+        include('../../config.php');
+    } else {
+        $errorMessage = "";
+        $errorMessage .= "PHP ERROR: config.php does not exist.";
+        echo $errorMessage;
+        exit;
+    }
 
-//Include Files
-if (file_exists('../../config.php')) {
-  include('../../config.php');
-} else {
-  $errorMessage = "";
-  $errorMessage .= "PHP ERROR: ../../config.php does not exist.";
-  echo $errorMessage;
-  exit;
-}
+        // functions page
+    if (file_exists('../../functions.php')) {
+        include('../../functions.php');
+    } else {
+        $errorMessage = "";
+        $errorMessage .= "PHP ERROR: functions.php does not exist.";
+        echo $errorMessage;
+        exit;
+    }
 
-if (file_exists('../../functions.php')) {
-  include('../../functions.php');
-} else {
-  $errorMessage = "";
-  $errorMessage .= "PHP ERROR: ../../functions.php does not exist.";
-  echo $errorMessage;
-  exit;
-}
 
-if (file_exists('../../library/db.class.php')) {
-  include('../../library/db.class.php');
-} else {
-  $errorMessage = "";
-  $errorMessage .= "PHP ERROR: ../../library/db.class.php does not exist.";
-  echo $errorMessage;
-  exit;
-}
+    if ($_SESSION["sessionStatus"] != 2 || empty($_SESSION["sessionStatus"])) {
+        header('Location: ' . ROOT_URL . 'index.php');
+    } else {
+
+        //header
+        if ($_SESSION["sessionStatus"] == 1) {
+            include "../../templates/header-user.php";
+        } else if ($_SESSION["sessionStatus"] == 2) {
+            include "../../templates/header-admin.php";
+        } else if ($_SESSION["sessionStatus"] == 6 || empty($_SESSION["sessionStatus"])) {
+            include "../../templates/header.php";
+        }
+    }
+?>
+
+<main class="shopping-cart">
+    <section class="shopping-cart">
+        <div class="shopping-cart-wrapper">
+            <h3>Uw Winkelmandje</h3>
+            <div class="shopping-cart-items">
+                <div class="shopping-cart-item">
+                    <div class="shopping-cart-info">
+                        <div class="product-image small-image item-margin">
+
+                        </div>
+                        <div class="shopping-cart-product-number item-margin">
+                            1
+                        </div>
+                        <div class="shopping-cart-product-title item-margin">
+                            Leren Werkschoenen
+                        </div>
+                        <div class="shopping-cart-product-price item-margin">
+                            $25
+                        </div>
+                        <div class="shopping-cart-product-quantity item-margin">
+                            2x
+                        </div>
+                    </div>
+                    <div class="shopping-cart-buttons">
+                        <div class="product-data button small small-icon black">
+                            <i class="fas fa-trash"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="order-button">
+                <button>Afrekenen</button>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php
 
 if ($_SESSION["sessionStatus"] == 1) {
-  include "../../templates/header-user.php";
+    include "../../templates/footer-user.php";
 } else if ($_SESSION["sessionStatus"] == 2) {
-  include "../../templates/header-admin.php";
+    include "../../templates/footer-admin.php";
 } else if ($_SESSION["sessionStatus"] == 6 || empty($_SESSION["sessionStatus"])) {
-  include "../../templates/header.php";
+    include "../../templates/footer.php";
 }
-
-$aWhereValue = array('p_id');
-
-// Getting top content from database
-$aColumnValue = array(1);
-$aShoppingShit = db::select("product", $aWhereValue, $aColumnValue, "i");
-
-var_dump($aShoppingShit);
-
-for ($i = 0; $i < 10; $i++) {
-  echo $aShoppingShit[$i] . "<br>";
-}
-
-
-
-
-
-
-
-if ($_SESSION["sessionStatus"] == 1) {
-  include "../../templates/footer-user.php";
-} else if ($_SESSION["sessionStatus"] == 2) {
-  include "../../templates/footer-admin.php";
-} else if ($_SESSION["sessionStatus"] == 6 || empty($_SESSION["sessionStatus"])) {
-  include "../../templates/footer.php";
-}
+?>
