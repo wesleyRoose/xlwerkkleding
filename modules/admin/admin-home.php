82404e727fd3f1bfd30fe2f1133e2c1ada80bf4e
StefanMaring <smaring@ziggo.nl>
2022-01-25 14:19:52 +0100

    ADMIN HOME

--- modules/admin/admin-home.php
+++ modules/admin/admin-home.php
@@ -15,27 +15,25 @@ if (file_exists('../../controller.php')) {
 
 <main class="admin-home">
     <section class="admin-home">
+        <h1>U bent ingelogd als Admin</h1>
         <div class="admin-home-wrapper">
             <?php
-                // Create query string
-                $sQuery = "SELECT * FROM `filterterms` WHERE `id` = " . $_SESSION["sessionAccountId"];
-
-                // Execute query and catch results in array
-                if ($oResult = $conn->query($sQuery)) {
-                    $aRow = $oResult->fetch_assoc();
-                }
-
                 // Create Query String
                 $sQuery = "SELECT * FROM `filterterms` WHERE `term` = 'category'";
                 // Execute query and catch result in array
                 if ($oResult = $conn->query($sQuery)) {
-                $sHtml = "<div>";
-                while ($aRow = $oResult->fetch_assoc()) {
-                    $sHtml .= "<div>" . $aRow["value"] . "</div>";
-                }
+                    $sHtml = "<div class='categories'>";
+                    $sHtml .= "<h3>Bestaande Categoriën</h3>";
+                    while ($aRow = $oResult->fetch_assoc()) {
+                        $sHtml .= "<div class='cs-item'>" . $aRow["value"] . 
+                        '<button class="product-data button small small-icon">
+                            <i class="fas fa-trash no-margin"></i>
+                        </button>
+                    </div>';
+                    }
                 }
 
-                $sCatagoryHtml = $sHtml;
+                $sCatagoryHtml = $sHtml . '</div>';
 
                 echo $sCatagoryHtml;
 
@@ -43,26 +41,19 @@ if (file_exists('../../controller.php')) {
                 $sQuery = "SELECT * FROM `filterterms` WHERE `term` = 'sector'";
                 // Execute query and catch result in array
                 if ($oResult = $conn->query($sQuery)) {
-                $sHtml = "<div>";
-                while ($aRow = $oResult->fetch_assoc()) {
-                    $sHtml .= "<div>" . $aRow["value"] . "</div>";
-                }
+                    $sHtml = "<div class='sectors'>";
+                    $sHtml .= "<h3>Bestaande Sectoren</h3>";
+                    while ($aRow = $oResult->fetch_assoc()) {
+                        $sHtml .= "<div class='cs-item'>" . $aRow["value"] . 
+                        '<button class="product-data button small small-icon">
+                            <i class="fas fa-trash no-margin"></i>
+                        </button>
+                    </div>';
+                    }
                 }
 
                 $sSectorHtml =  $sHtml . '</div>';
-
-                // Create Query String
-                $sQuery = "SELECT * FROM `filterterms` WHERE `term` = 'merk'";
-                // Execute query and catch result in array
-                if ($oResult = $conn->query($sQuery)) {
-                $sHtml = "";
-                while ($aRow = $oResult->fetch_assoc()) {
-                    $sHtml .= "<option value='" . $aRow["value"] . "'>" . $aRow["value"] . "</option>";
-                }
-                }
-
-                $sMerkHtml = $sHtml;
-
+                echo $sSectorHtml;
             ?>
         </div>
     </section>
