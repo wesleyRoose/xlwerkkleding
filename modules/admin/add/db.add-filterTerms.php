01a40935a41f134c3e58d8aa205d17de167b7342
stfokkert <52315931+stfokkert@users.noreply.github.com>
2022-01-27 09:14:09 +0100

    Update db.add-filterTerms.php

--- modules/admin/add/db.add-filterTerms.php
+++ modules/admin/add/db.add-filterTerms.php
@@ -1,8 +1,8 @@
 <?php
 
 //load controller
-if (file_exists('../../controller.php')) {
-  include('../../controller.php');
+if (file_exists('../../../controller.php')) {
+  include('../../../controller.php');
 } else {
   $errorMessage = "";
   $errorMessage .= "PHP ERROR: controller.php does not exist.";
