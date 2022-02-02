e009197cf56099e4ac49205bac94ba42a1f7295c
stfokkert <52315931+stfokkert@users.noreply.github.com>
2022-02-02 14:33:55 +0100

    d

--- modules/admin/product-overview/db.deleteProduct.php
+++ modules/admin/product-overview/db.deleteProduct.php
@@ -1,12 +1,18 @@
 <?php
 
-$sQuery = "DELETE FROM `products` WHERE `p_id` = " . $_GET["product"];
+//load controller
+if (file_exists('../../../admin-controller.php')) {
+  include('../../../admin-controller.php');
+} else {
+  $errorMessage = "";
+  $errorMessage .= "PHP ERROR: admin-controller.php does not exist.";
+  echo $errorMessage;
+  exit;
+}
+$sQuery = "DELETE FROM `product` WHERE `p_id` = " . $_GET["product"];
 
 //Execute query and catch results in array
 if ($db->query($sQuery)) {
-header('Location: ' . ROOT_URL . 'modules/admin/product-overview/index.php?msg=del');
-exit;
+  header('Location: ' . ROOT_URL . 'modules/admin/product-overview/index.php?msg=del');
+  exit;
 }
-
-
-?>
