1cbb5e373a28d2440a0281642884e5aec5e1a62b
StefanMaring <smaring@ziggo.nl>
2022-02-02 14:13:22 +0100

    Update index.php

--- modules/admin/product-delete/index.php
+++ modules/admin/product-delete/index.php
@@ -18,6 +18,19 @@ if (file_exists('../../../library/db.filterTerms.php')) {
   exit;
 }
 
+//Removes the product
+
+if(isset($_POST["del_user_btn"])) {
+  $sQuery = "DELETE FROM `users` WHERE `users`.`id` = " . $_SESSION["sessionAccountId"];
+
+  //Execute query and catch results in array
+  if ($db->query($sQuery)) {
+  session_destroy();
+  header('Location: ' . ROOT_URL . 'modules/login/index.php?msg=del');
+  exit;
+  }
+
+}
 
 if ($_SESSION["sessionStatus"] == 1) {
   include "../../../templates/footer-user.php";
