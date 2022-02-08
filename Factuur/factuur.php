a871a5939718adac24288431d66f6a12c5f0722a
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2022-02-08 15:26:26 +0100

    Factuur 6
    Factuur 6

--- Factuur/factuur.php
+++ Factuur/factuur.php
@@ -31,6 +31,8 @@ if (file_exists('../library/db.class.php')) {
 
 require('../factuur/fpdf.php');
 
+
+
 $pdf = new FPDF('P', 'mm', 'A4');
 
 $pdf->AddPage();
@@ -40,22 +42,30 @@ $pdf->SetFont('Arial','B',16);
 
 //logo 
 $pdf->image( '../img/xlwerkkleding-logo.jpeg', 10, 10, 30);
+
+//eerste lijn
 $pdf->line(10, 25, 200, 25);
 
 //Eerste table
 $pdf->Ln(20);//line break
-$pdf->Cell(55,5,'Reference code', 0, 0,);
-$pdf->Cell(58,5,': 026ETY', 0, 0, );
-$pdf->Cell(30,5,': Date', 0, 0, );
-$pdf->Cell(52,5,': time ', 0, 1, );
+$pdf->Cell(55,5,'Naam klant', 0, 0,);
+$pdf->Cell(49,5,': ', 0, 0, );
 
-$pdf->Cell(55,5,'Amount', 0, 0,);
-$pdf->Cell(58,5,'2674', 0, 0, );
-$pdf->Cell(30,5,': Channel', 0, 0, );
-$pdf->Cell(52,5,': Web ', 0, 1, );
+$pdf->Cell(45,5,'Postcode  : ', 0, 0,);
+$pdf->Cell(47,5,'7941 LZ Meppel', 0, 1, );
 
-$pdf->Cell(55,5,'Status', 0, 0, );
-$pdf->Cell(58,5,'Complete', 0, 1, );
+$pdf->Cell(55,5,'Datum', 0, 0,);
+$pdf->Cell(49,5,':', 0, 0, );
+
+$pdf->Cell(45,5,'Adres : ', 0, 0,);
+$pdf->Cell(49,5,'Synagogestraat 7 ', 0, 1, );
+
+
+$pdf->Cell(55,5,'Order nummer:', 0, 0, );
+$pdf->Cell(49,5,':', 0, 0, );
+
+$pdf->Cell(40,5,'Telefoon  : ', 0, 0,);
+$pdf->Cell(49,6,' +31 (0)6 120 55775', 0, 1, );
 
 // tweede table
 $pdf->Ln(20);//line break
@@ -71,13 +81,29 @@ $pdf->Cell(58,5,': 0', 0, 1, );
 $pdf->Cell(55,5,'Product Delivery', 0, 0, );
 $pdf->Cell(58,5,': 0', 0, 1, );
 
+//Tweede lijn
 $pdf->line(10, 60, 200, 60);
 
-//derde table
+//derder table
+$pdf->Ln(20);//line break
+$pdf->Cell(55, 5, 'Product id', 0, 0);
+$pdf->Cell(58, 5, ': 64351-84503', 0, 1);
+
+$pdf->Cell(55, 5, 'Tax Amount', 0, 0);
+$pdf->Cell(58, 5, ': 0 ', 0, 1);
+
+$pdf->Cell(55,5,'Product Service', 0, 0, );
+$pdf->Cell(58,5,': 0', 0, 1, );
+
+$pdf->Cell(55,5,'Product Delivery', 0, 0, );
+$pdf->Cell(58,5,': 0', 0, 1, );
+
+//vierde table
 $pdf->ln(10); //line break
 $pdf->Cell(55, 5, 'paid by:', 0, 0);
 $pdf->Cell(58, 5, '$acc_item_1', 0, 1);
 
+//derde lijn
 $pdf->line(10, 102, 200, 102);
 
 $pdf->ln(15); //line break
