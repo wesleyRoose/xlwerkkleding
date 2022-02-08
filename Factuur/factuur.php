a67733df585657f4dd2a7103a308518ddf31afde
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2022-02-08 13:10:35 +0100

    Factuur
    Factuur

--- Factuur/factuur.php
+++ Factuur/factuur.php
@@ -4,6 +4,7 @@ require('../factuur/fpdf.php');
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(100,20,'Hello World!', 1, 0, 'c' );
+$pdf->Cell(90,20,'Hello World 2', 1, 0, 'c');
 $pdf->Output();
 ?>
