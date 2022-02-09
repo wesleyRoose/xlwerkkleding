aa64845ac4df8bd346d22d2d6cf1f58d81b82b5d
wesleyRoose <74661132+wesleyRoose@users.noreply.github.com>
2022-02-09 14:38:03 +0100

    Factuur 8
    Factuur 8

--- Factuur/factuur.php
+++ Factuur/factuur.php
@@ -37,12 +37,12 @@ $pdf = new FPDF('P', 'mm', 'A4');
 $pdf->AddPage();
 
 // font
-$pdf->SetFont('Arial','B',16);
+$pdf->SetFont('arial','B',12);
 
 //logo 
 $pdf->image( '../img/xlwerkkleding-logo.jpeg', 10, 10, 30);
 
-//eerste lijn
+//eerste lijn onder logo
 $pdf->line(10, 25, 200, 25);
 
 //Eerste table
@@ -60,7 +60,7 @@ $pdf->Cell(45,5,'Adres : ', 0, 0,);
 $pdf->Cell(49,5,'Synagogestraat 7 ', 0, 1, );
 
 
-$pdf->Cell(55,5,'Order nummer:', 0, 0, );
+$pdf->Cell(55,5,'Order nummer', 0, 0, );
 $pdf->Cell(49,5,':', 0, 0, );
 
 $pdf->Cell(40,5,'Telefoon  : ', 0, 0,);
@@ -68,19 +68,12 @@ $pdf->Cell(49,6,' +31 (0)6 120 55775', 0, 1, );
 
 // tweede table
 $pdf->Ln(20);//line break
-$pdf->Cell(55, 5, 'Product id', 0, 0);
-$pdf->Cell(58, 5, ': 64351-84503', 0, 1);
+$pdf->Cell(40, 5, 'Aantal:', 0, 0);
+$pdf->Cell(90, 5, 'Omschrijving:', 0, 0);
+$pdf->Cell(40,5,'Per stuk:', 0, 0, );
+$pdf->Cell(55,5,'Totaal:', 0, 0, );
 
-$pdf->Cell(55, 5, 'Tax Amount', 0, 0);
-$pdf->Cell(58, 5, ': 0 ', 0, 1);
-
-$pdf->Cell(55,5,'Product Service', 0, 0, );
-$pdf->Cell(58,5,': 0', 0, 1, );
-
-$pdf->Cell(55,5,'Product Delivery', 0, 0, );
-$pdf->Cell(58,5,': 0', 0, 1, );
-
-//Tweede lijn
+//Tweede lijn onder de order nummer en telefoon
 $pdf->line(10, 60, 200, 60);
 
 //derder table
@@ -103,7 +96,7 @@ $pdf->Cell(55, 5, 'paid by:', 0, 0);
 $pdf->Cell(58, 5, '$acc_item_1', 0, 1);
 
 //derde lijn
-$pdf->line(10, 102, 200, 102);
+$pdf->line(10, 75, 200, 75);
 
 $pdf->ln(15); //line break
 $pdf->Cell(140, 15, '', 0, 0);
